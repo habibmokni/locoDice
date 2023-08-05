d85e1bfaaa93e34cdb7f8a8b5ca6e0b48c2e5081
habibmokni <habib.mokni@outlook.fr>
2023-08-05 10:29:32 +0200

    Implement Rolling the dice fnc

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+// Selecting Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+// Starting conditions
+let currentScore = 0;
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+//  Rolling Dice Functionality
+btnRoll.addEventListener('click', () => {
+  // 1, Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  // 2. Display Dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `assets/dice-${dice}.png`;
+  // 3. Check for Nr(1); if true, switch to next Player
+  if (dice !== 1) {
+    // add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; // TODO
+  } else {
+  }
+});
